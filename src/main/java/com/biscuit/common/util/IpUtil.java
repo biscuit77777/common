c084dcf3abe81c16c41dd17173ac39216fa77fed
mzh <biscuitv@163.com>
2024-12-13 09:15:46 +0800

    IpUtil

---
+++ src/main/java/com/biscuit/common/util/IpUtil.java
@@ -0,0 +1,28 @@
+package com.biscuit.common.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author biscuit
+ */
+public abstract class IpUtil {
+
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if (ip.contains(",")) {
+            return ip.split(",")[0];
+        } else {
+            return ip;
+        }
+    }
+
+}
