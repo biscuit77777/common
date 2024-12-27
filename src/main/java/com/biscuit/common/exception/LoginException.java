6fd9ee2d5423f943ffe104c214fac90875e14a93
mzh <biscuitv@163.com>
2024-12-27 14:06:00 +0800

    登录异常

--- src/main/java/com/biscuit/common/exception/LoginException.java
+++ src/main/java/com/biscuit/common/exception/LoginException.java
@@ -5,25 +5,25 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * 自定义异常类型
+ * 登录异常
  * @author biscuit
  */
 @Getter
 @Setter
-public class BiscuitException extends RuntimeException {
+public class LoginException extends RuntimeException {
 
     private String code;
     private String desc;
 
-    public BiscuitException() {
-        this(ResultEnum.FAIL);
+    public LoginException() {
+        this(ResultEnum.NOT_LOGIN);
     }
 
-    public BiscuitException(ResultEnum result) {
+    public LoginException(ResultEnum result) {
         this(result.getCode(), result.getMessage());
     }
 
-    public BiscuitException(String code, String message) {
+    public LoginException(String code, String message) {
         super(message);
         this.code = code;
         this.desc = message;
