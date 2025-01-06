f5a785344a1eaa34594455744bb560db20c3b374
mzh <biscuitv@163.com>
2025-01-06 11:25:31 +0800

    数值固定长度，补零

--- src/main/java/com/biscuit/common/util/NumberUtil.java
+++ src/main/java/com/biscuit/common/util/NumberUtil.java
@@ -44,4 +44,27 @@ public abstract class NumberUtil {
         return Objects.nonNull(number) && number.compareTo(BigDecimal.ZERO) > 0;
     }
 
+    /**
+     * 数值补零并转换为字符串
+     * @param digit 位数
+     * @param number 数值
+     */
+    public static String repairZeroToString(Integer digit, Integer number) {
+        return repairZeroToString(digit, Long.parseLong(number.toString()));
+    }
+
+    /**
+     * 数值补零并转换为字符串
+     * @param digit 位数
+     * @param number 数值
+     */
+    public static String repairZeroToString(Integer digit, Long number) {
+        AssertUtil.assertion(digit >= number.toString().length(), "数值位数大于补零总位数");
+        StringBuilder repair = new StringBuilder();
+        for (int i = 0; i < digit - number.toString().length(); i++) {
+            repair.append("0");
+        }
+        return repair.toString() + number;
+    }
+
 }
