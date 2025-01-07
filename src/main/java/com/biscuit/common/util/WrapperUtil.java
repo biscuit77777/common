fee64a4bdfe30570ff3b20a80ca81ddef3e920b0
mzh <biscuitv@163.com>
2025-01-07 19:44:57 +0800

    工具类完善

--- src/main/java/com/biscuit/common/util/WrapperUtil.java
+++ src/main/java/com/biscuit/common/util/WrapperUtil.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.biscuit.common.base.BaseEntity;
 import com.biscuit.common.enums.DeletedEnum;
 
+import java.time.LocalDateTime;
+
 /**
  * Wrapper 条件工具类
  * @author biscuit0
@@ -25,6 +27,7 @@ public abstract class WrapperUtil<T extends BaseEntity> {
     public static <T extends BaseEntity> LambdaUpdateWrapper<T> getDeleteWrapper(Class<T> clazz) {
         return Wrappers.lambdaUpdate(clazz)
                 .set(BaseEntity::getIsDeleted, DeletedEnum.TRUE.getValue())
+                .set(BaseEntity::getDeletedTime, LocalDateTime.now())
                 .eq(BaseEntity::getIsDeleted, DeletedEnum.FALSE.getValue());
     }
 
